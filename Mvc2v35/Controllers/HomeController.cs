95671be10af4ce84d7cade7751ecae551810acf0
xxxam <rogerjaeck-xxxam@yahoo.com>
2014-07-27 23:46:52 +0200

    Mvc2v35 added

---
+++ Mvc2v35/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc2v35.Controllers {
+  [HandleError]
+  public class HomeController : Controller {
+    public ActionResult Index() {
+      ViewData["Message"] = "Willkommen bei ASP.NET MVC";
+
+      return View();
+    }
+
+    public ActionResult About() {
+      return View();
+    }
+  }
+}
---
+++ Mvc2v35/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc2v35.MvcApplication" Language="C#" %>
