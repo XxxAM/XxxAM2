43d8d466eba0c7b69cc9e22cc808290ded954ff4
xxxam <rogerjaeck-xxxam@yahoo.com>
2014-07-30 22:45:35 +0200

    Unused code removed

--- SQL/FooSQL.sql
+++ SQL/FooSQL.sql
@@ -3,30 +3,6 @@
 USE Northwind
 GO
 
---SELECT od.*
---  FROM [dbo].[Orders] o
---  JOIN [dbo].[Order Details] od
---  ON o.OrderID = od.OrderID
-  --AND od.ProductID = (
-  --  SELECT TOP(1) [dbo].[Order Details].ProductID
-  --  FROM [dbo].[Order Details]
-  --  WHERE [dbo].[Order Details].[OrderID] = o.OrderID
-  --)
-
-
---SELECT x.OrderID, x.Discount 
---FROM [dbo].[Order Details] x
---JOIN (
---  SELECT od.OrderID
---  FROM [dbo].[Order Details] od
---  GROUP BY od.OrderID 
---) y ON y.OrderID = x.OrderID
-
---SELECT
---  od.OrderID
---FROM [dbo].[Order Details] od
---GROUP BY OrderID
-
 SELECT o.*, x.*
 FROM [dbo].[Orders] o
 JOIN 
@@ -43,7 +19,3 @@ JOIN
   ) AS y
   GROUP BY y.OrderID
 ) AS x ON x.OrderID = o.OrderID
-
---SELECT od.OrderID, count(*)
---FROM [dbo].[Order Details] od
---GROUP BY od.OrderID
